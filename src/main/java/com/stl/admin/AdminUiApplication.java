2888b54a277c69e710805d707c78fbbc1deab731
Sergio Torres Lozano <sergiotorreslozano@Sergios-MacBook-Air.local>
2021-06-03 21:46:56 +0200

    Initial commit

---
+++ src/main/java/com/stl/admin/AdminUiApplication.java
@@ -0,0 +1,15 @@
+package com.stl.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stl/admin/AdminUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stl.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
